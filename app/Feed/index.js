25eab7f3383bda5331400a8787b0dccf371d16d2
Fantasy Shao <forever.fantasy27@gmail.com>
2016-11-29 00:29:32 +0800

    Add <Feed /> <Category /> <Setting />

---
+++ app/Feed/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import {
+  Text,
+} from 'react-native';
+
+class Feed extends Component {
+  render() {
+    return (
+      <Text>Feed</Text>
+    );
+  }
+}
+
+export default Feed;
---
+++ app/Setting/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import {
+  Text,
+} from 'react-native';
+
+class Setting extends Component {
+  render() {
+    return (
+      <Text>Setting</Text>
+    );
+  }
+}
+
+export default Setting;
